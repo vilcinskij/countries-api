9e1bb12424eac94f52956e14d7806b6c36e45e3a
vilcinskij <vilcinskij@gmail.com>
2022-11-29 08:37:17 +0200

    užknisoNerealiai

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,8 +6,8 @@ const Navigation = () => {
   return (
     <div className='navigation'>
       <ul className='main-menu'>
-        <li className='menu-item'><Link to="./">Home</Link></li>
-        <li className='menu-item'><Link to="./countries">All countries</Link></li>
+        <li className='menu-item'><Link to="/">Home</Link></li>
+        <li className='menu-item'><Link to="/countries">All countries</Link></li>
         {/* <li className='menu-item'><Link to="/home">Continents</Link></li> */}
       </ul>
 
