075f5b2d517fbb3e96a04782015e0bf5f94e5d2b
vilcinskij <vilcinskij@gmail.com>
2022-12-01 23:21:00 +0200

    last

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -16,30 +16,6 @@ const HomePage = () => {
   return (
     <div>
       <SearchForm />
-      {/* <form id="login-form" className="login-form" onSubmit={submitHandler}>
-        <div className="form-element">
-          <label htmlFor="user-name" className="label text-label">Find a country:</label>
-          <input
-            type="text"
-            className="text-input"
-            name="country"
-            id="country-name"
-            placeholder="Enter country name"
-          />
-        </div>
-        <div className="form-element">
-          <input
-            type="submit"
-            name="submit"
-            id="submit-button"
-            value="SEARCH"
-            className="button login-button"
-          />
-        </div>
-      </form>
-      {country &&
-        <SearchResult result={country} />
-      } */}
     </div>
   )
 }
